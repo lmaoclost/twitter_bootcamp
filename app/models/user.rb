0fa5ebad86be599e5c393496959fc2f9951ebb5b
lmaoclost <renansmoliveira@gmail.com>
2018-08-19 13:12:01 -0300

    Followers Following

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
+
+  has_many :active_relationship, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationship, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+
+  has_many :followers, through: :active_relationship, source: :followed
+  has_many :following, through: :active_relationship, source: :followed
+  
 end
